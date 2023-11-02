587712f6da038c081f226c2630936ebbe3b27f41
Juul Hobert <juul.hobert@infosupport.com>
2023-11-02 20:50:44 +0100

    Initial commit

---
+++ src/JuulHobert.Blog.FunctionAppWithAppConfig/HelloWorld.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.FeatureManagement;
+
+namespace JuulHobert.Blog.FunctionAppWithAppConfig;
+
+public class HelloWorld
+{
+    private const string FeatureConfigName = "ConfigName";
+
+    private readonly IOptions<ServiceOptions> _options;
+    private readonly IFeatureManager _featureManager;
+
+    public HelloWorld(
+        IOptions<ServiceOptions> options,
+        IFeatureManager featureManager)
+    {
+        _options = options;
+        _featureManager = featureManager;
+    }
+
+    [FunctionName("HelloWorld")]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello-world")]
+        HttpRequest req)
+    {
+        var featureEnabled = await _featureManager.IsEnabledAsync(FeatureConfigName);
+        var content = featureEnabled ? $"Hello {_options.Value.Name}" : "Hello World!";
+
+        return new ContentResult
+        {
+            Content = content, ContentType = "text/plain"
+        };
+    }
+}
---
+++ src/JuulHobert.Blog.FunctionAppWithAppConfig/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Azure.Identity;
+using JuulHobert.Blog.FunctionAppWithAppConfig;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace JuulHobert.Blog.FunctionAppWithAppConfig;
+
+public class Startup : FunctionsStartup
+{
+    private const string AppConfigEndpointEnvironmentVariableName = "AppConfigEndpoint";
+
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+        var credentials = new DefaultAzureCredential();
+        var appConfigEndpoint = Environment.GetEnvironmentVariable(AppConfigEndpointEnvironmentVariableName);
+        if (string.IsNullOrEmpty(appConfigEndpoint))
+        {
+            throw new InvalidOperationException("AppConfigEndpoint is not set");
+        }
+
+        builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
+            options
+                .Connect(new Uri(appConfigEndpoint), credentials)
+                .Select($"{ServiceOptions.SectionName}:*")
+                .ConfigureKeyVault(kv => kv.SetCredential(credentials))
+                .UseFeatureFlags());
+    }
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services
+            .AddAzureAppConfiguration()
+            .AddFeatureManagement()
+            .Services
+            .AddOptions<ServiceOptions>()
+            .Configure<IConfiguration>((settings, configuration) =>
+            {
+                configuration.GetSection(ServiceOptions.SectionName).Bind(settings);
+            });
+    }
+}
+
+public class ServiceOptions
+{
+    public const string SectionName = "JuulHobertBlog";
+
+    public string Name { get; set; } = string.Empty;
+}
